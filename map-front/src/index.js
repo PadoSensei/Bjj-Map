367d0d6426a03c3aa457e158eff10ddc99a6edda
PadoSensei <padraigo.donoghue@gmail.com>
2022-01-10 13:47:18 +0000

    Registered and synced app with firebase

--- map-front/src/index.js
+++ map-front/src/index.js
@@ -2,19 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+//const API_KEY = {REACT_APP_FIREBASE_API}
+const firebaseConfig = {
+  apiKey: "AIzaSyBlfQS_fryKpqkXss0lhpPWf8tGTFs00hQ",
+  authDomain: "map-pad.firebaseapp.com",
+  projectId: "map-pad",
+  storageBucket: "map-pad.appspot.com",
+  messagingSenderId: "816706936700",
+  appId: "1:816706936700:web:746876a2bde67a2fb6cef9",
+  measurementId: "G-WZTVSVEPZ2"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(analytics)
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}
-  >
+  <React.StrictMode>
     <App />
-  </Auth0Provider>,
-  document.getElementById('root')
+  </React.StrictMode>,
+  rootElement
 );
 
--- map-front/src/ui-components/Profile.js
+++ map-front/src/ui-components/Profile.js
@@ -5,7 +5,7 @@ const Profile = () => {
   return (
     <div>
       <h3>This is the profile page.</h3>
-      <h3>Info on user, maps and classes.</h3>
+      <h3>Info on user, maps and nodes.</h3>
     </div>
     
   )
