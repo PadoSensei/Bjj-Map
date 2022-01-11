bd5bd06632a881cb7d1aaa9c2fcc4f1f6e9e308b
PadoSensei <padraigo.donoghue@gmail.com>
2022-01-11 15:45:21 +0000

    Home Component reverted to Class Component

--- map-front/src/index.js
+++ map-front/src/index.js
@@ -2,24 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-//const API_KEY = {REACT_APP_FIREBASE_API}
-const firebaseConfig = {
-  apiKey: "AIzaSyBlfQS_fryKpqkXss0lhpPWf8tGTFs00hQ",
-  authDomain: "map-pad.firebaseapp.com",
-  projectId: "map-pad",
-  storageBucket: "map-pad.appspot.com",
-  messagingSenderId: "816706936700",
-  appId: "1:816706936700:web:746876a2bde67a2fb6cef9",
-  measurementId: "G-WZTVSVEPZ2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-console.log(analytics)
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
--- map-front/src/ui-components/Home.js
+++ map-front/src/ui-components/Home.js
@@ -4,97 +4,67 @@ import Toolbar from './Toolbar';
 import Card from './Card';
 import css from './home.module.css';
 import router from '../router';
-// import LoginView from './LoginView';
-// import { useAuth0 } from '@auth0/auth0-react';
- 
-
-// Refactored from class component
-// Functional Component requires useEffect to rerender after creating / deleting maps.
-
-function Home (props) {
-  // const { user, isAuthenticated } = useAuth0();
-  const [list, setList] = React.useState(repository.getList({level: 0}));
-  const [id, setId] = React.useState(1);
-
-  // if (user){
-  //   console.log(user)
-  // }
-
-  // Work in Progress
-  React.useEffect(() => {
-    setList(repository.getList({level: 0}))
-  }, [list]);
+class Home extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          list: repository.getList({level: 0})
+      }
+  }
   
-  const actionMenu = [
-    { name: 'add', onClick: () => addNode() },
-    { name: 'delete', onClick: () => deleteNode(id) }
+  actionMenu = [
+    { name: 'add', onClick: () => this.addNode() },
+    { name: 'delete', onClick: () => this.deleteNode(this.state.id) }
   ];
 
   // Map Creation on Home Screen
-  const addNode = () => {
+  addNode = () => {
     repository.save({
       name: 'Rename idea',
       level: 0,
       parentId: null,
     });
-    setList({list: repository.getList({level: 0})})
+    this.setState({list: repository.getList({level: 0})});
   }
 
-  const setSelected = (id) => {
-    setId(id)
+  setSelected = (id) => {
+    this.setState({ id });
   }
 
-  const deleteNode = (id) => {
+  deleteNode = (id) => {
     repository.delete(id);
-    setList({list: repository.getList({level: 0})});
+    this.setState({ list: repository.getList({ level: 0 }) });
   }
 
   // Routes to selected map from Home Screen
-  const getMap = (id) => {
+  getMap = (id) => {
     router.setRoute('map', id);
   }
 
-  
+  render() {
     return (
       <>
         <h1>Home</h1>
-        <Toolbar list={actionMenu} type='alert' location={['vertical', 'right', 'bottom']} />
+        <Toolbar list={this.actionMenu} type='alert' location={['vertical', 'right', 'bottom']} />
         <div className={css.list}>
-          {/* Auth disabled for production with poor internet */}
-          {/* <div>
-            {!isAuthenticated && <LoginView isAuthenticated={isAuthenticated}/>}
-          </div>  */}
           {
-            list.map(item => (
-              <div className={css.item}key={item.id}>
-                <button onClick={() => getMap(item.id)}>Go to map...</button>
+            this.state.list.map(item => (
+              <div className={css.item} key={item.id}>
+                <button onClick={() => this.getMap(item.id)}>Go to map...</button>
                 <Card 
                   id={item.id} 
                   name={item.name} 
                   comment={item.comment} 
-                  isSelected={item.id === id}
-                  onClick={() => setSelected(item.id)}
+                  isSelected={item.id === this.state.id}
+                  onClick={() => this.setSelected(item.id)}
                 />
               </div>
             ))
           }
-          {/* {
-            isAuthenticated && list.map(item => (
-              <div className={css.item}key={item.id}>
-                <button onClick={() => getMap(item.id)}>Go to map...</button>
-                <Card 
-                  id={item.id} 
-                  name={item.name} 
-                  comment={item.comment} 
-                  isSelected={item.id === id}
-                  onClick={() => setSelected(item.id)}
-                />
-              </div>
-            ))
-          } */}
         </div>
       </>
     )
+  }
 }
 
 export default Home
