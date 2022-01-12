5da4faf3041ec8c4ba63e636a34fcb837c2506c5
PadoSensei <padraigo.donoghue@gmail.com>
2022-01-12 21:22:03 +0000

    LeftMenu Component Logout fix

--- map-front/src/ui-components/LeftMenu.js
+++ map-front/src/ui-components/LeftMenu.js
@@ -2,17 +2,16 @@ import React from 'react';
 import css from './leftmenu.module.css';
 import router from '../utils/router';
 import Toolbar from './Toolbar';
-import { useAuth0 } from '@auth0/auth0-react';
+
 
 function LeftMenu(props) {
-  const { logout } = useAuth0();
-
+  
   const list =[
     { name: 'home', onClick: () => router.setRoute('home') },
     { name: 'profile', onClick: () => router.setRoute('profile') },
     { name: 'study', onClick: () => router.setRoute('study') },
     { name: 'help', onClick: () => router.setRoute('help') },
-    { name: 'logout', onClick: () => logout()}
+    { name: 'logout', onClick: () => alert("This will trigger a logout.")}
   ];
 
   let className = css.container;
